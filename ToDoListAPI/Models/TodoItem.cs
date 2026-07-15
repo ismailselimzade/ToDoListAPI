69ba0e28ee5ed471cee1838cde25ea34a7f35866
Ismayil Selimzade <ismailselimzade@mail.ru>
2026-07-15 19:35:54 +0400

    feat(models): add User and TodoItem entities

---
+++ ToDoListAPI/Models/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoListAPI.Models
+{
+    public enum Status { Pending, InProgress, Completed }
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Title { get; set; }
+        public Status Status { get; set; }
+    }
+}
