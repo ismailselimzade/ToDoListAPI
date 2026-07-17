4c5b1857b70d6cb4bf900fcc3952e3173540f1b6
Ismayil Selimzade <ismailselimzade@mail.ru>
2026-07-17 16:31:19 +0400

    feat(dto): add CreateUserDto and GetUsersDto

---
+++ ToDoListAPI/DTOs/CreateUserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ToDoListAPI.DTOs
+{
+    public class CreateUserDto
+    {
+        public string UserName { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
---
+++ ToDoListAPI/DTOs/GetUsersDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ToDoListAPI.DTOs
+{
+    public class GetUsersDto
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
