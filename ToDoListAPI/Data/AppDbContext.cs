3c5aabf8282395daba3e676df463d96434d5b1c4
Ismayil Selimzade <ismailselimzade@mail.ru>
2026-07-17 16:30:49 +0400

    refactor(data): make DbSet properties public in AppDbContext

--- ToDoListAPI/Data/AppDbContext.cs
+++ ToDoListAPI/Data/AppDbContext.cs
@@ -5,8 +5,8 @@ namespace ToDoListAPI.Data
 {
     public class AppDbContext : DbContext
     {
-        DbSet<User> Users { get; set; }
-        DbSet<TodoItem> TodoItems { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<TodoItem> TodoItems { get; set; }
 
         public AppDbContext (DbContextOptions<AppDbContext> options) : base(options) { }
     }
