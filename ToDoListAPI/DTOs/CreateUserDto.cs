c8143c5164206dc445b3f753203fe96c175debf6
Ismayil Selimzade <ismailselimzade@mail.ru>
2026-07-18 15:20:11 +0400

    fix: rename userDto password property

--- ToDoListAPI/DTOs/CreateUserDto.cs
+++ ToDoListAPI/DTOs/CreateUserDto.cs
@@ -3,6 +3,6 @@
     public class CreateUserDto
     {
         public string UserName { get; set; }
-        public string PasswordHash { get; set; }
+        public string Password { get; set; }
     }
 }
