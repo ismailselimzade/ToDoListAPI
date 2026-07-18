ca7895a2069f58fc90ca587f6270df2e1ba9009d
Ismayil Selimzade <ismailselimzade@mail.ru>
2026-07-18 16:16:38 +0400

    feat: add UpdateUserDto

---
+++ ToDoListAPI/DTOs/UpdateUserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoListAPI.DTOs
+{
+    public class UpdateUserDto
+    {
+        public string UserName { get; set; }
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
